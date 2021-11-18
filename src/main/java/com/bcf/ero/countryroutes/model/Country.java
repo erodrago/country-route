20c409815151365cb39d5d5ad724666f9ebfc5a4
Eric Ndirangu Mwangi <emwangi14@safaricom.co.ke>
2021-11-18 08:47:17 +0300

    chore: created model classes

--- src/main/java/com/bcf/ero/countryroutes/model/Country.java
+++ src/main/java/com/bcf/ero/countryroutes/model/Country.java
@@ -1,2 +1,19 @@
-package com.bcf.ero.countryroutes.model;public class Country {
+package com.bcf.ero.countryroutes.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Country {
+
+    @JsonProperty(value = "cca3")
+    private String name;
+
+    private List<String> borders;
 }
--- src/main/java/com/bcf/ero/countryroutes/model/Route.java
+++ src/main/java/com/bcf/ero/countryroutes/model/Route.java
@@ -1,2 +1,12 @@
-package com.bcf.ero.countryroutes.model;public class Route {
+package com.bcf.ero.countryroutes.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Route {
+    private List<String> path;
 }
