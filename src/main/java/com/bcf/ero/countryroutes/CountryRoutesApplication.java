df753f4aabf74ebeacb3617fa90459a2decf0bb0
Eric Ndirangu Mwangi <emwangi14@safaricom.co.ke>
2021-11-18 07:15:41 +0300

    initial commit

---
+++ src/main/java/com/bcf/ero/countryroutes/CountryRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.bcf.ero.countryroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryRoutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryRoutesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcf/ero/countryroutes/CountryRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcf.ero.countryroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryRoutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
